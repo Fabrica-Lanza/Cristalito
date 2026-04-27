8e01ced80be21f6f722f396f9b1e3052508d0b9e
mceciliavalinotto-cristalito <mceciliavalinotto@gmail.com>
2026-04-27 13:56:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cristalito-v39';
+const CACHE_NAME = 'cristalito-v41';
 const urlsToCache = ['/Cristalito/', '/Cristalito/index.html', '/Cristalito/manifest.json'];
 
 self.addEventListener('install', event => {
