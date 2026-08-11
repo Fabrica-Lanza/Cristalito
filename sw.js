07621d82d3a8799387af43a3773899c1aaf8c3f1
mceciliavalinotto-cristalito <mceciliavalinotto@gmail.com>
2026-08-11 14:45:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cristalito-v53';
+const CACHE_NAME = 'cristalito-v54';
 const urlsToCache = ['/Cristalito/', '/Cristalito/index.html', '/Cristalito/manifest.json'];
 
 self.addEventListener('install', event => {
